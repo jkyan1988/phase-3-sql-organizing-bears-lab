f469c5495435461baa3e43b0d3b9932a3ca376fe
James Kyan <james.kyan88@gmail.com>
2022-07-01 14:26:18 -0400

    commit tree 5fec9b821208785bd4a2aa7ae8382645eae5b4f1

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 12, 'F', 'Purple', 'happy', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 13, 'M', 'Violet', 'sad', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 15, 'F', 'Aqua', 'anxious', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 42, 'F', 'Brown', 'confused', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 42, 'M', 'White', 'angry', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 2, 'M', 'Red', 'oblivious', false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 64, 'M', 'Green', 'senile', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (null, 64, 'M', 'Blue', 'senile', true);
--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,19 +1,60 @@
 def selects_all_female_bears_return_name_and_age
-  "Write your SQL query here"
+  <<-SQL
+    SELECT
+      bears.name,
+      bears.age
+    FROM
+      bears
+    WHERE
+      sex='F';
+  SQL
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "Write your SQL query here"
+  <<-SQL
+    SELECT
+      bears.name
+    FROM
+      bears
+    ORDER BY
+      bears.name;
+  SQL
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "Write your SQL query here"
+  <<-SQL
+    SELECT
+      bears.name,
+      bears.age
+    FROM
+      bears
+    WHERE
+      alive=true
+    ORDER BY
+      bears.age;
+  SQL
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  <<-SQL
+    SELECT
+      bears.name,
+      bears.age
+    FROM
+      bears
+    WHERE
+      age > 19 AND bears.name IS NOT NULL
+    SQL
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  <<-SQL
+    SELECT
+      bears.name,
+      bears.age
+    FROM
+      bears
+    WHERE
+      age < 3
+    SQL
 end
